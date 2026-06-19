ca87a08fc72dab58bf8e96803c514d02e35e55c9
Federico Midaglia <fmidaglia@gocloud.la>
2026-06-18 22:42:24 -0300

    feat(files): reorder datasources

--- lambdas.tf
+++ lambdas.tf
@@ -34,6 +34,46 @@ locals {
   cognito_lambdas_vpc = merge(flatten(local.cognito_lambdas_vpc_tmp)...)
 }
 
+data "aws_vpc" "cognito_lambda" {
+  for_each = local.cognito_lambdas_vpc
+
+  filter {
+    name   = "tag:Name"
+    values = [each.value.vpc_name]
+  }
+}
+
+data "aws_subnets" "cognito_lambda" {
+  for_each = local.cognito_lambdas_vpc
+
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.cognito_lambda[each.key].id]
+  }
+
+  tags = {
+    Name = each.value.subnet_name
+  }
+
+  lifecycle {
+    postcondition {
+      condition     = length(self.ids) > 0
+      error_message = "No subnets found in VPC '${each.value.vpc_name}' with Name tag '${each.value.subnet_name}' for lambda '${each.key}'."
+    }
+  }
+}
+
+data "aws_security_group" "cognito_lambda" {
+  for_each = local.cognito_lambdas_vpc
+
+  vpc_id = data.aws_vpc.cognito_lambda[each.key].id
+
+  tags = {
+    Name = each.value.security_group
+  }
+}
+
+
 module "cognito_lambdas" {
   for_each = local.cognito_lambdas
   source   = "terraform-aws-modules/lambda/aws"
